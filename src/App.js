7c5ca8920c4e7c4e24b1412bbdf64533e610b3d3
Saroj-Sah <sahsaroj615@gmail.com>
2022-12-20 22:16:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './Components/ChatFeed';
+import LoginForm from './Components/LoginForm';
+import './App.css';
+
+const projectID = '9eb1143d-cad2-4a5b-8804-0cd41c411e88';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+
+export default App;
